cc155bd5ddf907540ab3de6e66e1c2254e4484e2
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2026-01-20 18:11:08 +0530

    refactor: consolidate role and permission tables into a single migration

--- migrations/001_create_users.sql
+++ migrations/001_create_users.sql
@@ -1,6 +1,8 @@
 --CREATE TABLE USERS
 
-CREATE TYPE department_type AS ENUM ('IT','product', 'hr', 'sales')
+CREATE TYPE department_type AS ENUM ('IT','product', 'hr', 'sales');
+CREATE TYPE role_type AS ENUM ('admin', 'editor', 'viewer');
+
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -10,4 +12,36 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name role_type NOT NULL UNIQUE,
+    description TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
 
+
+
+--assigns roles to users
+CREATE TABLE user_roles (
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    role_id UUID NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE permissions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    action TEXT NOT NULL,  
+    resource_type TEXT NOT NULL,  
+    description TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(action, resource_type) 
+);
+
+-- which roles have which permissions
+CREATE TABLE role_permissions (
+    role_id UUID NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+    permission_id UUID NOT NULL REFERENCES permissions(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (role_id, permission_id)
+);
